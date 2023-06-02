e736529bf8e8182964c30fce165bbc069ef23f1d
Thinh <thinhletruongquoc66@gmail.com>
2023-06-02 22:34:26 +0700

    update product

--- admincp/modules/quanlysp/them.php
+++ admincp/modules/quanlysp/them.php
@@ -2,36 +2,36 @@
 <table border="1" width="100%" style="border-collapse:collapse">
 <form method="POST" action="modules/quanlysp/xuly.php" enctype="multipart/form-data">
   <tr>
-    <td>Tên sản phẩm</td>
+    <td>Tên sản phẩm:</td>
     <td><input type="text" name="tensanpham"></td>
   </tr>
   <tr>
-    <td>Mã sản phẩm</td>
+    <td>Mã sản phẩm:</td>
     <td><input type="text" name="masp"></td>
   </tr>
   <tr>
-    <td>Giá sản phẩm</td>
+    <td>Giá sản phẩm:</td>
     <td><input type="text" name="giasp"></td>
   </tr>
   <tr>
-    <td>Số lượng</td>
+    <td>Số lượng:</td>
     <td><input type="text" name="soluong"></td>
   </tr>
   <tr>
-    <td>Hình ảnh</td>
+    <td>Hình ảnh:</td>
     <td><input type="file" name="hinhanh"></td>
   </tr>
   <tr>
-    <td>Tóm tắc</td>
+    <td>Tóm tắc:</td>
     <td><textarea rows="10"  name="tomtac" style="resize: none;"></textarea></td>
   </tr>
   <tr>
-    <td>Nội dung</td>
+    <td>Nội dung:</td>
     <td><textarea rows="10"  name="noidung" style="resize: none;"></textarea></td>
   </tr>
 
   <tr>
-    <td>Danh mục sản phẩm</td>
+    <td>Danh mục sản phẩm:</td>
     <td>
       <select name="danhmuc">
         <?php
@@ -39,7 +39,7 @@
         $query_danhmuc = mysqli_query($mysqli,$sql_danhmuc);
         while($row_danhmuc = mysqli_fetch_array($query_danhmuc)){
         ?>
-        <option value="<?php echo $row_danhmuc['id_danhmuc']?>"><?php echo $row_danhmuc['tendanhmuc'] ?></option>
+        <option value="<?php echo $row_danhmuc['id_danhmuc']?>"><?php echo $row_danhmuc['tendanhmuc']?></option>
         <?php
         }
         ?>
